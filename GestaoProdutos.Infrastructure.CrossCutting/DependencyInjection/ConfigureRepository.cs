f0c5df8aeab09c6b6ef9e464e799c47202ca7da9
davipromanha <davip.romanha@gmail.com>
2022-02-17 04:04:58 -0300

    Foi adicionado o projeto Application e parte da injeção de dependência.

---
+++ GestaoProdutos.Infrastructure.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,23 @@
+﻿using GestaoProdutos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestaoProdutos.Infrastructure.CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection,
+                                                        IConfiguration configuration)
+        {
+            var secaoBanco = configuration.GetSection("DatabaseOptions");
+            var connection = $"Server={secaoBanco.GetSection("Server").Value}" +
+                $";Database={secaoBanco.GetSection("Database").Value}" +
+                $";User Id={secaoBanco.GetSection("UserId").Value}" +
+                $";Password={secaoBanco.GetSection("Password").Value}" +
+                ";Persist Security Info=True;";
+
+            serviceCollection.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
+        }
+    }
+}
---
+++ GestaoProdutos.Infrastructure.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,15 @@
+﻿using GestaoProdutos.Dominio.Repositorio.Interfaces.Services;
+using GestaoProdutos.Infrastructure.Data.Repositorios;
+using GestaoProdutos.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestaoProdutos.Infrastructure.CrossCutting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IServicoProdutos, ProdutoServices>();
+        }
+    }
+}
